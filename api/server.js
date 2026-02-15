b758455bed732ec1493d5fe938d3e5600b65e45a
Touseef Ahmad <ta4760877@gmail.com>
2026-02-16 00:08:07 +0500

    fixed database issue

--- api/server.js
+++ api/server.js
@@ -7,9 +7,15 @@ import { createClient } from "@vercel/kv";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
 const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
+
+const kv = createClient({
+  url: process.env.KV_REST_API_URL,
+  token: process.env.KV_REST_API_TOKEN,
+});
 
 
 // Shorten URL endpoint
